7e4536811e10cea9d9eeadf50bd124e299811a53
Nate Mara <natemara@fastmail.com>
2021-02-28 13:16:03 -0800

    Autoformat code

--- game-watch.js
+++ game-watch.js
@@ -4,51 +4,56 @@ const KEY = '__dominionGame.state';
 const URL = 'https://dominion-scores.app.natemara.com/chart';
 
 function save() {
-    localStorage.setItem(KEY, JSON.stringify(game));
+  localStorage.setItem(KEY, JSON.stringify(game));
 }
 
 function load() {
-    const json = localStorage.getItem(KEY);
-    if (json) {
-        game = JSON.parse(json);
+  const json = localStorage.getItem(KEY);
+  if (json) {
+    game = JSON.parse(json);
 
-        resetIfGameChanged();
-    } else {
-        reset();
-    }
+    resetIfGameChanged();
+  } else {
+    reset();
+  }
 }
 
 function resetIfGameChanged() {
-    if (game.id !== gameId()) {
-        reset();
-    }
+  if (game.id !== gameId()) {
+    reset();
+  }
 }
 
 function gameId() {
-    if (activeGame) {
-        return activeGame.getId();
-    }
+  if (activeGame) {
+    return activeGame.getId();
+  }
 
-    return -1;
+  return -1;
 }
 
 function reset() {
-    game = { id: gameId(), startTime: new Date().getTime(), scores: {}, turnChanges: [] };
-    save();
+  game = {
+    id: gameId(),
+    startTime: new Date().getTime(),
+    scores: {},
+    turnChanges: [],
+  };
+  save();
 }
 
 function timeDelta() {
-    return new Date().getTime() - game.startTime;
+  return new Date().getTime() - game.startTime;
 }
 
 function recordingMessage() {
-    const sideBar = document.querySelector('.side-bar');
+  const sideBar = document.querySelector('.side-bar');
 
-    const elt = document.createElement('div');
-    elt.style.color = 'red';
-    elt.innerText = 'This game is being recorded';
+  const elt = document.createElement('div');
+  elt.style.color = 'red';
+  elt.innerText = 'This game is being recorded';
 
-    sideBar.insertBefore(elt, sideBar.firstChild);
+  sideBar.insertBefore(elt, sideBar.firstChild);
 }
 
 load();
@@ -58,53 +63,53 @@ const $rootScope = angular.element(document.body).injector().get('$rootScope');
 
 let turn;
 $rootScope.$on('newTurn', (_, turnData) => {
-    resetIfGameChanged();
+  resetIfGameChanged();
 
-    if (turn !== turnData.turnNumber) {
-        turn = turnData.turnNumber;
+  if (turn !== turnData.turnNumber) {
+    turn = turnData.turnNumber;
 
-        game.turnChanges.push(timeDelta());
-        save();
-    }
+    game.turnChanges.push(timeDelta());
+    save();
+  }
 });
 
 $rootScope.$on('counterChange', (_, counterData) => {
-    resetIfGameChanged();
+  resetIfGameChanged();
 
-    const counter = activeGame.state.counters[counterData.index];
+  const counter = activeGame.state.counters[counterData.index];
 
-    if (counter && counter.counterName && counter.counterName.name === 'Points') {
-        const points = counter.value;
-        const player = activeGame.model.players[counter.owner];
+  if (counter && counter.counterName && counter.counterName.name === 'Points') {
+    const points = counter.value;
+    const player = activeGame.model.players[counter.owner];
 
-        if (player) {
-            const playerName = player.name;
+    if (player) {
+      const playerName = player.name;
 
-            const scores = game.scores[playerName] || { scores: [], times: [] };
+      const scores = game.scores[playerName] || { scores: [], times: [] };
 
-            scores.scores.push(points);
-            scores.times.push(timeDelta());
+      scores.scores.push(points);
+      scores.times.push(timeDelta());
 
-            game.scores[playerName] = scores;
-            save();
-        }
+      game.scores[playerName] = scores;
+      save();
     }
+  }
 });
 
 $rootScope.$on('gameFinished', async () => {
-    const gameJson = JSON.stringify(game);
+  const gameJson = JSON.stringify(game);
 
-    await fetch(URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: gameJson,
-    });
-    reset();
+  await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: gameJson,
+  });
+  reset();
 });
 
 $rootScope.$on('gameStarted', () => {
-    recordingMessage();
-    reset();
+  recordingMessage();
+  reset();
 });
